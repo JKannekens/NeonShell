a293cb61f7aaed9d3a1e792bbaba03dbc60d78e4
Justin Kannekens <justinkannekens@gmail.com>
2025-02-04 15:40:23 +0100

    Initial terminal setup

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-terminal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './terminal.component.html',
-  styleUrl: './terminal.component.scss'
+  styleUrl: './terminal.component.scss',
 })
 export class TerminalComponent {
+  @ViewChild('terminalInput') textAreaRef!: ElementRef<HTMLTextAreaElement>;
+  currentInput: string = '';
+  cursorPosition = 0;
 
+  executeCommand() {
+    console.log(this.currentInput);
+  }
 }
